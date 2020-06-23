ecac46874121ee2afc8239b8acefcd03969938a5
Timothy Banks <timothy.banks@block.one>
2020-06-23 14:46:53 -0400

    Rollback overzealous use of std::make_optional

--- libraries/fc/include/fc/variant.hpp
+++ libraries/fc/include/fc/variant.hpp
@@ -350,7 +350,7 @@ namespace fc
         explicit variant( const std::optional<T>& v )
         {
            memset( this, 0, sizeof(*this) );
-           if( v ) *this = variant(*v);
+           if( v.has_value() ) *this = variant(*v);
         }
 
         template<typename T>
@@ -390,7 +390,7 @@ namespace fc
    template<typename T>
    void from_variant( const variant& var,  std::optional<T>& vo )
    {
-      if( var.is_null() ) vo = std::make_optional<T>();
+      if( var.is_null() ) vo = std::optional<T>();
       else
       {
           vo = T();
--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -2078,7 +2078,7 @@ struct resolver_factory {
             }
          }
 
-         return std::make_optional<abi_serializer>();
+         return std::optional<abi_serializer>();
       };
    }
 };
